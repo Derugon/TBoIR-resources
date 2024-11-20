8c17184bfb3882a0b18e27c250a8570f1364cdee
Adrien LESÉNÉCHAL <adrien.lesenechal@etu.univ-nantes.fr>
2024-11-20 10:08:27 +0100

    remove tools

--- 1.06.J127/tools/LuaDocs/search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['remarks',['Remarks',['../md__d_1_doxygen_test_input_converted_test__remarks.html',1,'']]]
-];
--- 1.06.J127/tools/LuaDocs/search/pages_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['writing_20screen_20shaders',['Writing screen shaders',['../md__d_1_doxygen_test_input_converted_test__screen__shaders.html',1,'']]]
-];
